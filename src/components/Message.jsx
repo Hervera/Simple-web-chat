31b8c4d2853ab915d21a51671646e7a42514a5ed
Herve Nkurikiyimfura <nkuliherve@gmail.com>
2022-03-30 09:43:15 +0200

    improve users messages ui

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
-function Message({ username, message, sentAt }) {
+function Message({ username, tabUsername, message, sentAt }) {
     const firstLetterOfUsername = username || '';
-
+    const isTabUser = username === tabUsername;
     return (
-        <div className="chat-message-left pb-4">
+        <div className={isTabUser ? 'chat-message-right mb-4' : 'chat-message-left pb-4'}>
             <div>
-                <span className="avatar">
+                <span className={`avatar ${isTabUser ? 'dark-blue' : 'white-black'}`}>
                     {firstLetterOfUsername.charAt(0)}
                 </span>
-                
             </div>
-            <div className="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
-                <div className="font-weight-bold mb-1">{username}</div>
-                <div>{message}</div>
-                <div className="text-muted small text-nowrap mt-2">{sentAt}</div>
+            <div className={isTabUser ? 'mr-2' : 'ml-2'}>
+                <div className="flex-shrink-1 bg-light rounded py-2 px-3">
+                    <div className="font-weight-bold mb-1 size-14">{username}</div>
+                    <p className="m-0 py-0 size-14">{message}</p>
+                </div>
+                <div className="text-muted small text-nowrap mt-1">{sentAt}</div>
             </div>
         </div>
     )
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Message from "./Message";
 
 
-const Messages = ({ messages, isLoading }) => {
+const Messages = ({ messages, isLoading, tabUsername }) => {
     return (
         <div className="position-relative">
             {isLoading ? (<h5 className="loading  text-center my-3">Loading ...</h5>) : (
                 <div className="chat-messages p-4">
                     {messages.map(message => 
-                        <Message key={message.id} username={message.username} message={message.message} sentAt={message.sentAt} />
+                        <Message 
+                            key={message.id} 
+                            username={message.username} 
+                            message={message.message} 
+                            sentAt={message.sentAt} 
+                            tabUsername={tabUsername}
+                        />
                     )}
                 </div>
             )}
